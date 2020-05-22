5fab873e628a5752b984dec2d0a278e00400dcf6
Siva Reddybathula <siva.reddybathula@gmail.com>
2020-05-21 23:32:33 -0700

    Intial setup (#2)
    Working first copy of bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,14 @@
 def bubble_sort(_list):
+    swapped = True
+    while(swapped):
+        swapped = False
+        for i in range(0, len(_list)-1):
+           if(_list[i] > _list[i+1]):
+               _list[i], _list[i+1] = _list[i+1], _list[i]
+               swapped = True
     return _list
 
 if __name__ == "__main__":
     import random
     print(bubble_sort([random.randint(-1000,1000) for x in range(500)]))
+
